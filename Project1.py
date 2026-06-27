dc6dd2cb304858485ab03130691770c9ce84525f
Damilolaonome <Onome.umukoro2@gmail.com>
2026-06-27 14:13:49 -0700

    Initial commit

---
+++ Project1.py
@@ -0,0 +1,5 @@
+#Day 1 project Band Name Generator
+print("Welcome to the Band Name Generator!")
+city = input("What city did you grew up in?\n")
+pet = input("What's the name of your pet?\n")
+print("Your band name could be:" + city + " " + pet)
