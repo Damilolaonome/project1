71723d60b86c28535678ddaa1064588ef478ea15
Damilolaonome <Onome.umukoro2@gmail.com>
2026-06-28 07:35:19 -0700

    Updated Day 2 project Tip calculator

--- umukoro_onome_project2.py
+++ umukoro_onome_project2.py
@@ -1,3 +1,4 @@
+#Day 2 project tip calculator
 print("Welcome to the tip calculator!")
 bill = float(input("What was the total bill? $"))
 tip = int(input("What percentage tip would you like to give? 10 12 15 "))
